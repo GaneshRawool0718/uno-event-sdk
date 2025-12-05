dffe5e17526efbb80c55e609cfaf6dc5bc1dbb75
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-12-05 17:09:34 +0530

    feat(exception): add EventDispatchException for dispatch errors

---
+++ src/Exception/EventDispatchException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Uno\EventSdk\Exception;
+
+/**
+ * Thrown when the SDK cannot dispatch an event through Messenger.
+ */
+class EventDispatchException extends EventSdkException
+{
+}
